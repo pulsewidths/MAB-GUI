5bf15fe224ebfd1877672c7221035a5eefea89ad
Hunter Pressley <hunter.s.pressley@gmail.com>
2022-07-09 03:45:40 -0400

    Refactored signals, cleaned up some formatting.

--- index.js
+++ index.js
@@ -1,292 +1,344 @@
-const electron = require('electron');
-const url = require('url');
-const plugin_manager = require('./JS/plugin_manager.js');
-const path = require('path');
+const electron = require( 'electron' );
+const url = require( 'url' );
+const pluginManager = require( './JS/plugin_manager.js' ); // @todo: change filename
+const path = require( 'path' );
+const { constants } = require('http2');
 const ipcMain = electron.ipcMain;
 
-const {app, BrowserWindow, Menu} = electron;
+const { app, BrowserWindow, Menu } = electron;
 
-// SET ENV (removes developer tools)
-// process.env.NODE_ENV = 'production';
+// dev tools; comment this code to turn on/off.
+process.env.NODE_ENV = 'development';
 
 let window;
-var place_args = 'global';
-var component_args = 'global';
-var transition_args = 'global';
-var stub_args = 'global'
+var placeArgs = 'global'; // @todo: argh... all of this... why?
+var componentArgs = 'global';
+var transitionArgs = 'global';
+var stubArgs = 'global';
 
-function boot() {
-	// Create new window
-	window = new BrowserWindow({
-		icon: path.join(__dirname, './Icons/logo.ico'),
-		webPreferences: {
+// on loading background stuff, run 'boot' function.
+app.on( 'ready', boot );
+
+function boot( )
+{
+	// create window.
+	window = new BrowserWindow(
+	{
+		icon: path.join( __dirname, './Icons/logo.ico' ),
+		webPreferences:
+		{
 			nodeIntegration: true,
 			contextIsolation: false
 		}
-	});
-	window.maximize()
-	window.loadURL(url.format({
-		pathname: path.join(__dirname, './HTML/index.html'),
+	} );
+
+	// load initial canvas page.
+	window.loadURL( url.format(
+	{
+		pathname: path.join( __dirname, './HTML/index.html' ),
 		protocol: 'file:',
 		slashes: true
-	}));
+	} ) );
 
-	// Populate Plugins Dropdown with valid Plugins
-	populate_plugins();
+	// create default menu from 'mainMenu' variable below.
+	Menu.setApplicationMenu( mainMenu )
 
-	//Quit app when closed
-	window.on('closed', function() {
-		app.quit()
-	});
+	populatePlugins( );
 
-	// Build menu from template
-	const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-	// Insert menu
-	Menu.setApplicationMenu(mainMenu)
+	// maximize window.
+	window.maximize( )
+
+	// cleanly close app.
+	window.on( 'closed',
+		function( )
+		{
+			app.quit( )
+		}
+	);
+
+	createSignalReceivers( ); // @todo: is this positioned correctly?
+
 };
 
-function populate_plugins() {
-	// Add all valid plugins to the Plugins menu
-	for (var menu = 0; menu < mainMenuTemplate.length; menu++) {
-		// Ensuring the location of Plugins menu
-		if (mainMenuTemplate[menu].label == 'Plugins') {
-			for (var index = 0; index < plugin_manager[0].length; index++) {
-
-				// New Plugin Constructed
-				const new_plugin = {
-					label: plugin_manager[0][index],
-					accelerator: 'CmdOrCtrl+' + index,
-					driver_path: plugin_manager[1][index],
-					plugin_number: index,
-					message: plugin_manager[0][index].toLocaleLowerCase().replace(/ /g,"_"),
-					click(MenuItem){
-						window.webContents.send(MenuItem.message);
-						console.log('The following Plugin has been activated: ' + MenuItem.label);
-					}
-				}
-
-				// Adding New Plugin to the Plugins menu
-				mainMenuTemplate[menu].submenu.push(new_plugin);
+const mainMenu = Menu.buildFromTemplate(
+	[
+		{
+			label: 'File',
+			submenu: [
+			{
+				// quit + hotkey.
+				label: 'Quit',
+				accelerator:
+					process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+					click( ) { app.quit( ); }
 			}
+			]
+		},
+		{
+			label: 'Plugins',
+			submenu: [ ]
 		}
+	]
+);
+
+const simulatorMenu = Menu.buildFromTemplate(
+	[
+		{
+			label: 'File',
+			submenu: [
+			{
+				// quit + hotkey.
+				label: 'Quit',
+				accelerator: // @todo: why is this formatted differently than below?
+					process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+					click( ) { app.quit( ); }
+			}
+			]
+		},
+		{
+			label: 'Exit Simulator Mode',
+				click( )
+				{
+					// send 'exit' signal to renderer.
+					window.webContents.send( 'exitSimulator' ); // @todo: change exit_simulator to exitSimulator elsewhere.
+					// reset menu.
+					Menu.setApplicationMenu( mainMenu );
+				}
+		}
+	]
+);
+
+// add all valid plugins to the 'plugins' menu dropdown.
+function populatePlugins( )
+{
+
+	// locate the 'plugins' dropdown in the main menu.
+	var pluginsMenuIndex = 0;
+	while( pluginsMenuIndex < mainMenu.items.length )
+	{
+
+		if( mainMenu.items[ pluginsMenuIndex ].label == 'Plugins' )
+		{
+			break;
+		}
+
+		pluginsMenuIndex++;
+
 	}
+	
+	// populate from pluginManager.
+	var currentPluginNum = 0; // @todo: better name?
+	while( currentPluginNum < pluginManager[ 0 ].length ) // @todo: what is this array structure? @todo: change name of pluginManager elsewhere.
+	{
+
+		const plugin = new electron.MenuItem
+		( {
+			label: pluginManager[ 0 ][ currentPluginNum ],
+			accelerator: 'CmdOrCtrl+' + currentPluginNum, // @todo: increment by one? @todo: why is this accelerator format different than above?
+			driverPath: pluginManager[ 1 ][ currentPluginNum ],
+			pluginNumber: currentPluginNum,
+			message: pluginManager[ 0 ][ currentPluginNum ].toLocaleLowerCase( ).replace( / /g, '_' ),
+				click( MenuItem )
+				{
+					window.webContents.send( MenuItem.message );
+					console.log( 'The ' + MenuItem.label + ' plugin has been activated.' );
+				}
+		} );
+
+		console.log( mainMenu.items[ pluginsMenuIndex ] );
+		mainMenu.items[ pluginsMenuIndex ].submenu.append( plugin );
+
+		currentPluginNum++;
+
+	}
+
 }
 
-const mainMenuTemplate = [
-	{
-		label: 'File',
-		submenu:[
-			{
-				label: 'Quit',
-				// Keyboard shortcuts for quit
-				accelerator: process.platform == 'darwin' ? 'Command+Q' :
-				'Ctrl+Q',
-				click(){
-					app.quit();
-				}
-			}
-		]
-	},
-	{
-		label: 'Plugins',
-		submenu:[]
-	}
-];
-
-const simulatorMenuTemplate = [
-	{
-		label: 'File',
-		submenu:[
-			{
-				label: 'Quit',
-				// Keyboard shortcuts for quit
-				accelerator: process.platform == 'darwin' ? 'Command+Q' :
-				'Ctrl+Q',
-				click(){
-					app.quit();
-				}
-			}
-		]
-	},
-	{
-		label: 'Exit Simulator Mode',
-		click() {
-			// Send message to renderer to exit simulator mode
-			window.webContents.send('exit_simulator_mode');
-			// Rebuild menu from template
-			const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-			// Insert menu
-			Menu.setApplicationMenu(mainMenu);
+// if mac, add empty object to menu. @todo: reposition this?
+if( process.platform == 'darwin' )
+{
+	mainMenu.items.unshift(
+		{
+	  		label: app.getName( ),
+	  		submenu: [ { role: 'quit' } ]
 		}
-	}
-];
-
-// if mac, add empty object to menu
-if(process.platform == 'darwin'){
-	mainMenuTemplate.unshift({
-	  label: app.getName(),
-	  submenu: [
-	  { role: 'quit' }
-	  ]
-	})
+	);
  }
 
-// Add developer tools item if not in production
-if(process.env.NODE_ENV !== 'production'){
-	mainMenuTemplate.push({
-		label: 'Developer Tools',
-		submenu:[
-			{
-				label: 'Toggle DevTools',
-				// Keyboard shortcuts for DevTools
-				accelerator: process.platform == 'darwin' ? 'Command+I' :
-				'Ctrl+I',
-				click(item, focusedWindow){
-					focusedWindow.toggleDevTools();
+// add developer tools. @todo: reposition this?
+if( process.env.NODE_ENV == 'development' )
+{
+
+	mainMenu.append( new electron.MenuItem( 
+		{
+			label: 'Developer Tools',
+			submenu: [
+				{
+					label: 'Toggle DevTools',
+					// dev tools + hotkey.
+					accelerator:
+						process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+						click( item, focusedWindow )
+						{
+							focusedWindow.toggleDevTools( );
+						}
+				},
+				{
+					role: 'reload'
 				}
-			},
-			{
-				role: 'reload'
-			}
-		]
-	});
+			]
+		}
+	) );
+
 }
 
-// Catch enter simulator mode click
-ipcMain.on('enter_simulator_mode', function(args) {
-	// Rebuild menu from template
-	const simulatorMenu = Menu.buildFromTemplate(simulatorMenuTemplate);
-	// Insert menu
-	Menu.setApplicationMenu(simulatorMenu);
-})
+function createSignalReceivers( )
+{
+	ipcMain.on( 'enterSimulator',
+		function( args )
+		{
+			Menu.setApplicationMenu( simulatorMenu ); // @todo: what exactly does this do?
+		}
+	);
 
-// Catch dependency port right clk
-ipcMain.on("set_dependency_type", function(event, args) {
-	console.log("Logging: set_dependency_type from main thread");
-	// Create new dialog
-	const {dialog} = require('electron');
+	ipcMain.on( 'setDependencyType', // @todo: move this over to the root of each relevant .js file?
+		function( event, args )
+		{
+			console.log( "Logging: setDependencyType from main thread." );
+			const { dialog, MenuItem } = require( 'electron' );
+			const options = {
+				// type: 'question',
+				buttons: [ 'Cancel', 'SERVICE', 'DATA' ],
+				title: 'Question',
+				message: 'Choose a dependency type: '
+			};
 
-	const options = {
-		//type: 'question',
-		buttons: ['Cancel', 'SERVICE', 'DATA'],
-		title: 'Question',
-		message: 'What kind of dependency port do you wish to create?'
-	};
+			// @todo: clean this up somehow...?
+			var dependencyType;
+			let response = dialog.showMessageBox( options );
+			switch( response )
+			{
+				case 0:
+					dependencyType = 'Cancel';
+					break;
+				case 1:
+					dependencyType = 'Service';
+					break;
+				case 2:
+					dependencyType = 'Data';
+					break;
+				default:
+					dependencyType = 'Cancel';
+			}
+			event.returnValue = dependencyType;
+		}
+	);
 
-	//Synchronous usage
-	let response = dialog.showMessageBox(options)
-	console.log(response)
-	var dependency_type;
-	switch(response){
-		case 0:
-			dependency_type = 'cancel';
-			break;
-		case 1:
-			dependency_type = 'service';
-			break;
-		case 2:
-			dependency_type = 'data';
-			break;
-		default:
-			dependency_type = 'cancel';
-	}
-	event.returnValue = dependency_type;
-});
+	// place right-click.
+	ipcMain.on( 'changePlaceDetails',
+		function( event, args )
+		{
+			console.log( 'Logging: changePlaceDetails from main thread.' );
+			placeArgs = args; // @todo: ?? why is this global?
+			// create new window.
+			var placeWindow = new BrowserWindow( { width: 350, height: 200 } );
+			placeWindow.loadURL( url.format( {
+				pathname: path.join( __dirname, './HTML/changePlaceDetails.html' ), // @todo: update filename.
+				protocol: 'file:',
+				slashes: true
+			} ) );
+		}
+	);
 
-// Catch place right click
-ipcMain.on("change_place_details", function(event, args) {
-	console.log("Logging: change_place_details from main thread");
-	place_args = args;
-	// Create new window
-	var place_window = new BrowserWindow({
-		width: 350,
-		height: 200
-	})
-	place_window.loadURL(url.format({
-		pathname: path.join(__dirname, './HTML/change_place_details.html'),
-		protocol: 'file:',
-		slashes: true
-	}));
-});
+	// @todo: can this be compacted?
+	ipcMain.on( 'place->main',
+		function( event, args )
+		{
+			window.webContents.send( 'place->renderer', { component: placeArgs.component, place: placeArgs.place, name: args.name } );
+		}
+	);
 
-ipcMain.on("place->main", function(event, args) {
-	window.webContents.send("place->renderer", {component: place_args.component, place: place_args.place, name: args.name});
-});
+	ipcMain.on( 'changeComponentDetails',
+		function( event, args )
+		{
+			console.log( 'Logging: changeComponentDetails from main thread.' );
+			componentArgs = args; // @todo: ?? why is this global?
+			// create window.
+			var componentWindow = new BrowserWindow( { width: 350, height: 200 } );
+			componentWindow.loadURL( url.format( {
+				pathname: path.join( __dirname, '.HTML/changeComponentDetails.html' ), // @todo: change filename elsewhere.
+				protocol: 'file:',
+				slashes: true
+			} ) );
 
-// Catch component right click
-ipcMain.on("change_component_details", function(event, args) {
-	console.log("Logging: change_component_details from main thread");
-	component_args = args;
-	// Create new window
-	var component_window = new BrowserWindow({
-		width: 350,
-		height: 200
-	})
-	component_window.loadURL(url.format({
-		pathname: path.join(__dirname, './HTML/change_component_details.html'),
-		protocol: 'file:',
-		slashes: true
-	}));
-});
+		}
+	);
 
-ipcMain.on("component->main", function(event, args) {
-	window.webContents.send("component->renderer", {component_name: component_args.component_name, name: args.name});
-});
+	ipcMain.on( 'component->main',
+		function( event, args )
+		{
+			window.webContents.send( 'component->renderer', { componentName: componentArgs.componentName, name: args.name } ); // @todo: ??? what is this structure?
+		}
+	);
 
-// Catch transition right click
-ipcMain.on("change_transition_details", function(event, args) {
-	console.log("Logging: change_transition_details from main thread");
-	transition_args = args;
-	// Create new window
-	var transition_window = new BrowserWindow({
-		width: 470,
-		height: 275
-	})
-	transition_window.loadURL(url.format({
-		pathname: path.join(__dirname, './HTML/change_transition_details.html'),
-		protocol: 'file:',
-		slashes: true
-	}));
-});
+	ipcMain.on( 'changeTransitionDetails',
+		function( event, args )
+		{
+			console.log( 'Logging: changeTransitionDetails from main thread.' );
+			transitionArgs = args; // @todo: why is this global?
+			var transitionWindow = new BrowserWindow( { width: 470, height: 275 } );
+			transitionWindow.loadURL( url.format( {
+				pathname: path.join( __dirname, './HTML/changeTransitionDetails.html' ), // @todo: change filename elsewhere.
+				protocol: 'file:',
+				slashes: true
+			} ) );
+		}
+	);
 
-ipcMain.on("transition->main", function(event, args) {
-	window.webContents.send("transition->renderer", {component: transition_args.component, transition: transition_args.transition, old_name: transition_args.name,
-													 name: args.name, old_func: transition_args.function,  new_func: args.function, 
-													 new_duration_min: args.duration_min, new_duration_max: args.duration_max});
-});
+	ipcMain.on( 'transition->main',
+		function( event, args )
+		{
+			window.webContents.send( 'transition->renderer', // @todo: this is ugly.
+				{ component: transitionArgs.component, transition: transitionArgs.transition, oldName: transitionArgs.name,
+				  name: args.name, oldFunc: transitionArgs.function, newFunc: args.function, newDurationMin: args.durationMin, newDurationMax: args.durationMax }
+				);
+		}
+	);
 
-// Catch stub right click
-ipcMain.on("change_stub_details", function(event, args) {
-	console.log("Logging: change_stub_details from main thread");
-	stub_args = args;
-	// Create new window
-	var stub_window = new BrowserWindow({
-		width: 350,
-		height: 200
-	})
-	stub_window.loadURL(url.format({
-		pathname: path.join(__dirname, './HTML/change_stub_details.html'),
-		protocol: 'file:',
-		slashes: true
-	}));
-});
+	ipcMain.on( 'changeStubDetails',
+		function( event, args )
+		{
+			console.log( 'Logging: changeStubDetails from main thread.' );
+			stubArgs = args; // @todo: ?? why is this global?
+			// create new window.
+			var stubWindow = new BrowserWindow( { width: 350, height: 200 } );
+			stubWindow.loadURL( url.format( {
+				pathname: path.join( __dirname, './HTML/changeStubDetails.html' ),
+				protocol: 'file:',
+				slashes: true
+			} ) );
+		}
+	);
 
-ipcMain.on("stub->main", function(event, args) {
-	console.log(args.name);
-	window.webContents.send("stub->renderer", {component: stub_args.component, old_name: stub_args.stub, new_name: args.name});
-});
+	ipcMain.on( 'stub->main',
+		function( event, args )
+		{
+			console.log( args.name );
+			window.webContents.send( 'stub->renderer', { component: stubArgs.component, oldName: stubArgs.stub, newName: args.name } ); // @todo: this is ugly.
+		}
+	);
 
-// Catch user manual plugin activation
-ipcMain.on('open_user_manual_window', function(event, args) {
-	// Create new user manual window
-	var user_manual_window = new BrowserWindow()
-	user_manual_window.setMenuBarVisibility(false)
-	user_manual_window.loadURL(url.format({
-		pathname: path.join(__dirname, './Plugins/User Manual/user_manual.html'),
-		protocol: 'file:',
-		slashes: true
-	}));
-});
+	ipcMain.on( 'openUserManualWindow',
+		function( event, args )
+		{
+			var userManualWindow = new BrowserWindow( );
+			userManualWindow.setMenuBarVisibility( false );
+			userManualWindow.loadURL( url.format( {
+				pathname: path.join( __dirname, './Plugins/User Manual/userManual.html' ), // @todo: change filename elsewhere.
+				protocol: 'file:',
+				slashes: true
+			} ) );
+		}
+	);
 
-// Listen for app to be ready
-app.on('ready', boot);
+}
