3dd41c13edff0dddb463844165cb226d330a0753
Hunter Pressley <hunter.s.pressley@gmail.com>
2022-07-25 00:26:10 -0400

    Component class now handles everything component-related.

--- JS/component.js
+++ JS/component.js
@@ -1,262 +1,268 @@
-// Adds a new component to the stage
-function addNewComponent(posX, posY) {
+class Component
+{
 
-    // create a new component group every time a component is created
-    var component_group = new Konva.Group({
-        x: posX,
-        y: posY,
-        width: 300,
-        height: 350,
-        draggable: true,
-        name: 'component_group'
-    });
+    constructor( name, posX, posY ) // @todo: is type required...?
+    {
 
-    // create the konva node
-    var component = new Konva.Rect({
-        x: 0,
-        y: 0,
-        width: 300,
-        height: 350,
-        stroke: 'black',
-        name: 'component',
-        strokeWidth: 0.5
-    });
+        this.type = 'component';
+        this.name = name;
+        this.index = componentList.length;
 
-    // selection area used for created USE dependences from this component
-    var use_selection_area = new Konva.Rect({
-        x: 0,
-        y: 0,
-        width: 15,
-        height: 350,
-        opacity: 0,
-        name: 'use_selection_area'
-    });
+        this.placeList = [ ];
+        this.transitionList = [ ];
+        this.transitionDictionary = { };
+        this.dependencyList = [ ];
 
-    // selection area used for created USE dependences from this component
-    var provide_selection_area = new Konva.Rect({
-        x: component.getWidth() - 15,
-        y: 0,
-        width: 15,
-        height: 350,
-        opacity: 0,
-        name: 'provide_selection_area'
-    });
+        this.initKonva( posX, posY );
+        this.initTooltip( );
+        this.initListeners( );
+        
+    }
 
-    // tooltip to display name of object
-    var tooltip = new Konva.Text({
-        text: "",
-        fontFamily: "Calibri",
-        fontSize: 12,
-        padding: 5,
-        textFill: "white",
-        fill: "black",
-        alpha: 0.75,
-        visible: false
-    });
+    initKonva( posX, posY )
+    {
 
-    var tooltipLayer = new Konva.Layer();
-    tooltipLayer.add(tooltip);
-    stage.add(tooltipLayer);
+        this.group = new Konva.Group( {
+            x: posX, y: posY,
+            width: 300, height: 350,
+            draggable: true, name: 'componentGroup'
+        } );
+        this.shape = new Konva.Rect( {
+            x: 0, y: 0,
+            width: 300, height: 350,
+            stroke: 'black', name: 'component',
+            strokeWidth: 0.5
+        } );
 
-    // create a component object and add it to the global list
-    var component_obj = new Component('Component', "Component_" + generateNextIndex(component_list));
-    component_obj.index = generateNextIndex(component_list);
-    component_obj.component_group_konva = component_group;
-    component_obj.konva_component = component;
-    component_obj.use_selection_area = use_selection_area;
-    component_obj.provide_selection_area = provide_selection_area;
-    component_obj.tooltipLayer = tooltipLayer;
-    component_list.push(component_obj);
+        this.useSelectionArea = new Konva.Rect( {
+            x: 0, y: 0,
+            width: 15, height: 350,
+            opacity: 0, name: 'useSelectionArea'
+        } );
+        this.provideSelectionArea = new Konva.Rect( {
+            x: this.shape.getWidth( ) - 15, y: 0,
+            width: 15, height: 350,
+            opacity: 0, name: 'provideSelectionArea'
+        } );
 
-    component_group.add(component);
-    component_group.add(use_selection_area);
-    component_group.add(provide_selection_area);
-    layer.add(component_group);
-    layer.draw();
+        this.group.add( this.shape );
+        this.group.add( this.useSelectionArea );
+        this.group.add( this.provideSelectionArea );
 
-    // event1: on left-click
-    stage.on('click', function (e) {
+        globalLayer.add( this.group ); // @todo: should this be a parameter?
+        globalLayer.draw( );
 
-        if (e.evt.button === 0) {
+    }
 
-            // if click on empty area - remove all transformers
-            if (e.target === stage) {
-                stage.find('Transformer').destroy();
-                layer.draw();
-                return;
+    initTooltip( )
+    {
+
+        this.tooltip = new Konva.Text( {
+            text: '', fontFamily: 'Calibri', fontsize: 12,
+            padding: 5, textFill: 'white', fill: 'black',
+            alpha: 0.75, visible: false
+        } );
+
+        this.tooltipLayer = new Konva.Layer( );
+        this.tooltipLayer.add( this.tooltip );
+        globalStage.add( this.tooltipLayer ); // @todo: should this be a parameter...? 
+
+    }
+
+    initListeners( )
+    {
+
+        this.initLeftClickListeners( ); // single & double.
+        this.initRightClickListeners( );
+        this.initMovementListeners( );
+
+    }
+
+    initLeftClickListeners( )
+    {
+        
+        // single-click on stage.
+        // @todo: should this be somewhere else...?
+        globalStage.on( 'click',
+            function( event )
+            {
+                if( event.evt.button === 0 )
+                {
+                    // if clicking on empty area...
+                    if( event.target === globalStage )
+                    {
+                        globalStage.find( 'Transformer' ).destroy( ); // deselect
+                        globalLayer.draw( );
+                        return;
+                    }
+
+                    // if clicking on a component...
+                    if( event.target.hasName( 'component' ) )
+                    {
+
+                        // remove any current selection
+                        stage.find( 'Transformer' ).destroy( );
+
+                        var transformer = new Konva.Transformer( {
+                            rotateEnabled: false,
+                            enabledAnchors: [ 'middle-right', 'bottom-center', 'bottom-right' ]
+                        } );
+
+                        event.target.getParent( ).add( transformer );
+                        transformer.attachTo( event.target );
+                        globalLayer.draw( );
+                        
+                    }
+
+                }
             }
+        );
 
-            // do nothing if clicked NOT on our rectangles
-            if (!e.target.hasName('component')) {
-                return;
+        // weirdness from js's anonymous scoping
+        var component = this;
+
+        // double-click in component.
+        this.shape.on( 'dblclick',
+            function( event )
+            {
+
+                if( event.evt.button === 0 )
+                {
+
+                    var transform = component.shape.getParent( ).getAbsoluteTransform( ).copy( );
+                    // get relative position
+                    transform.invert( );
+                    var pos = globalStage.getPointerPosition( );
+                    var placePos = transform.point( pos );
+                    var placeObj = addNewPlace( this, placePos ); // @todo: global function?
+
+                    // turn last line into constructor?
+                    // var placeObj = new Place( this, placePos );
+
+                    globalLayer.draw( );
+
+                }
+            } );
+
+    }
+
+    initRightClickListeners( )
+    {
+
+        var component = this;
+
+        // when this component is right-clicked...
+        this.shape.on( 'click',
+            function( event )
+            {
+                if( event.evt.button === 2 )
+                {
+
+                    // highlight
+                    component.shape.stroke( 'blue' );
+                    component.shape.strokeWidth( 3 );
+                    component.shape.draw( );
+
+                    ipcRend.send( 'changeComponentDetails',
+                        { componentName: component.name } );
+
+                }
             }
+        );
 
-            // remove old transformers
-            // TODO: we can skip it if current rect is already selected
-            stage.find('Transformer').destroy();
+    }
 
-            // create new transformer
-            var tr = new Konva.Transformer({
-                rotateEnabled: false,
-                enabledAnchors: ['middle-right', 'bottom-center', 'bottom-right'] // ¯\_(ツ)_/¯
-            });
-            e.target.getParent().add(tr);
-            tr.attachTo(e.target);
-            layer.draw();
+    initMovementListeners( )
+    {
 
-        }
+        var component = this;
 
-    });
+        this.shape.on( 'xChange yChange',
+            function( )
+            {
+                component.useSelectionArea.position(
+                    { x: shape.getX( ),
+                      y: shape.getY( ) } );
+                component.useSelectionArea.height( component.shape.getHeight( ) * component.shape.scaleY( ) );
+                
+                component.provideSelectionArea.position(
+                    { x: component.shape.getX( ) + ( component.shape.getWidth( ) * component.shape.scaleX( ) ) - 15,
+                      y: component.shape.getY( ) } );
+                      component.provideSelectionArea.height( component.shape.getHeight( ) * component.shape.scaleY( ) );
+            } );
 
-    // event2: on right-click
-    component.on("click", function(e){
+        this.group.on( 'dragmove',
+            function( )
+            {
+                component.tooltip.hide( );
+                component.tooltipLayer.draw( );
+            } );
 
-        if (e.evt.button === 2){
+        this.group.on( 'dragend',
+            function( )
+            {
+                component.group.position( {
+                    x: snapToGrid( component.group.x( ) ),
+                    y: snapToGrid( component.group.y( ) )
+                } )
+                globalLayer.batchDraw( );
+            } );
+        
+        this.shape.on( 'mousemove',
+            function( )
+            {
+                var mousePos = globalStage.getPointerPosition( );
+                component.tooltip.position( { x: mousePos.x + 10, y: mousePos.y + 10 } );
+                component.tooltip.text( component.name );
+                component.tooltip.show( );
+                component.tooltipLayer.batchDraw( );
+            } );
 
-            // highlight the component
-            component.stroke('blue');
-            component.strokeWidth(3);
-            component.draw();
+        this.shape.on( 'mouseover',
+            function( )
+            {
+                window.addEventListener( 'keydown', component.deletorPrompt );
+            } );
 
-            // open window for editing
-            ipcRend.send("change_component_details", {component_name: component_obj.name});
+        this.shape.on( 'mouseout',
+            function( )
+            {
+                component.shape.stroke( 'black' );
+                component.shape.strokeWidth( 1 );
+                component.tooltip.hide( );
+                component.tooltipLayer.draw( );
+                window.removeEventListener( 'keydown', component.deletorPrompt );
+            } );
 
-        };
+        
 
-    });
+    }
 
-    // event3: on double left-click
-    component.on('dblclick', function (e){
+    deletorPrompt( event )
+    {
 
-        if (e.evt.button === 0){
-
-            // what is transform of parent element?
-            var transform = component.getParent().getAbsoluteTransform().copy();
-
-            // to detect relative position we need to invert transform
-            transform.invert();
-
-            // now we find relative point
-            var pos = stage.getPointerPosition();
-            var placePos = transform.point(pos);
-
-            // grow component here
-            var place_obj = addNewPlace(component_obj, placePos);
-
-            layer.draw();
-
-        }
-
-    });
-
-    // event4: on component position changed
-    component.on('xChange yChange', function () {
-
-        // set use selection area position on component move or scale
-        use_selection_area.position({
-            x: component.getX(),
-            y: component.getY()
-        });
-
-        use_selection_area.height(component.getHeight() * component.scaleY());
-
-        // set provide selection area position on component move or scale
-        provide_selection_area.position({
-            x: component.getX() + (component.getWidth() * component.scaleX()) - 15,
-            y: component.getY()
-        })
-
-        provide_selection_area.height(component.getHeight() * component.scaleY());
-
-    });
-
-    // event5: on component being moved
-    component_group.on('dragmove', (e) => {
-
-        tooltip.hide();
-        tooltipLayer.draw();
-
-    });
-
-    // event6: on drag end
-    component_group.on('dragend', (e) => {
-
-        component_group.position({
-          x: snapToGrid(component_group.x()),
-          y: snapToGrid(component_group.y())
-        });
-
-        layer.batchDraw();
-
-    });
-
-    // event7: on mouse move within component
-    component.on('mousemove', function () {
-
-        var mousePos = stage.getPointerPosition();
-
-        tooltip.position({
-            x : mousePos.x + 10,
-            y : mousePos.y + 10
-        });
-
-        tooltip.text(component_obj.name);
-        tooltip.show();
-        tooltipLayer.batchDraw();
-
-    });
-
-    // event8: on mouse over component
-    component.on("mouseover", function(e){
-
-        window.addEventListener('keydown', removeComponent);
-
-    });
-
-    // event9: on mouse out
-    component.on("mouseout", function(){
-
-        component.stroke('black');
-        component.strokeWidth(1);
-        tooltip.hide();
-        tooltipLayer.draw();
-        window.removeEventListener('keydown', removeComponent);
-
-    });
-
-    function generateNextIndex(component_list) {
-
-        if(component_list.length == 0) {
-            return 1;
-        } else {
-            return component_list[component_list.length-1].index+1;
+        if( event.keyCode === 46 || event.keyCode === 8 )
+        {
+            if( confirm( 'Deleting this component will remove everything inside of it - are you sure?' ) )
+            {
+                deletor( this );
+            }
         }
 
     }
 
-    function removeComponent(ev){
+}
 
-        // keyCode Delete key
-        if (ev.keyCode === 46 || ev.keyCode == 8) {
+function addNewComponent( posX, posY )
+{
 
-            if (confirm('Are you sure you want to delete this Component? You will lose everything inside of it.')){
-                // Delete it!
-                deletor(component_obj);
-                //removeComponentObj(component_obj);
-                // component_obj.component_group_konva.destroy();
-                // layer.batchDraw();
-            } else {
-                // Do nothing!
-                return;
-            }
+    // create a component object and add it to the global list
+    var component = new Component( "Component_" + componentList.length, posX, posY );
+    componentList.push( component );
 
-        }
+    return component;
 
-    };
-
-    return component_obj;
-
-};
+}
 
 // Catch new component name from ipcMain
 ipcRend.on("component->renderer", function(event, args) {
