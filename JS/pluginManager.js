a36ac9f01dbbad073ca473c7abb6fc52f263ffa0
Hunter Pressley <hunter.s.pressley@gmail.com>
2022-08-18 20:27:19 -0400

    Fixed plugin population

--- JS/pluginManager.js
+++ JS/pluginManager.js
@@ -1,26 +1,28 @@
 const fs = require('fs');
 const path = require('path');
 
-const plugins_folder = path.join(__dirname, '../Plugins/');
+const pluginsFolder = path.join(__dirname, '../Plugins/');
 
-let valid_plugins = [];
-let valid_paths = [];
+let validPlugins = [];
 
-//Collects the plugins present in the Plugins folder
-let plugins = fs.readdirSync(plugins_folder);
-console.log('\nThe following ' + plugins.length + ' Plugin(s) have been Detected:\n    ' + plugins);
+let subFolders = fs.readdirSync( pluginsFolder );
 
-for (var i = 0; i < plugins.length; i++) {
-    if (fs.lstatSync(plugins_folder + plugins[i] + '/').isDirectory()){
-        let files = fs.readdirSync(plugins_folder + plugins[i] + '/');
+for ( let index = 0; index < subFolders.length; index++ )
+{
+    if( fs.lstatSync( pluginsFolder + subFolders[ index ] + '/').isDirectory( ) )
+    {
+        let files = fs.readdirSync( pluginsFolder + subFolders[ index ] + '/' );
 
-        if (files.includes('driver.js')) {
-            valid_plugins.push(plugins[i]);
-            valid_paths.push(plugins_folder + plugins[i] + '/');
+        if ( files.includes('driver.js') )
+        {
+            validPlugins.push(
+                { name: subFolders[ index ],
+                  path: pluginsFolder + subFolders[ index ] + '/' }
+            );
         }
+
     }
+
 }
 
-console.log('\nThe following ' + valid_plugins.length + ' Plugin(s) have been Validated:\n    ' + valid_plugins);
-
-module.exports = [valid_plugins, valid_paths];
+module.exports = validPlugins;
--- JS/script.js
+++
@@ -1,7 +0,0 @@
-// Function to change stub name
-function changeStubName(component, stub_name, new_stub_name) {
-    // find the component obj
-    var found_component_obj = component_list.find(function(element) { return element.name == component; });
-    var found_dependency_obj = found_component_obj.dependency_list.find(function(element) { return element.name == stub_name; });
-    if (found_dependency_obj){ found_dependency_obj.name = new_stub_name; }
-};
