76e1b2207a1c1de3eff20a8ff40b467a2f80574c
Hunter Pressley <hunter.s.pressley@gmail.com>
2022-08-20 00:26:09 -0400

    Cleaner code generation.

--- Plugins/Generate Code/driver.js
+++ Plugins/Generate Code/driver.js
@@ -1,193 +1,171 @@
-const electron = require('electron');
-const ipcRenderer = electron.ipcRenderer;
+const fs = require( 'fs' );
 
-var fs = require('fs'); // Load the File System to execute our common tasks (CRUD)
-var app = electron.remote; 
-var dialog = app.dialog;
-var comp_list = componentList;
-var con_list = connectionList;
+function generate( mabGUI )
+{
 
-ipcRenderer.on('generate_code', function() {
-    /** Loop through the component list, for every component create a new file
-     * string, open the dialog box, save that string to the newly chosen file 
-     */
-    for (var i = 0; i < comp_list.length; i++) {
-        createComponentString(comp_list[i]);
-    };
-    createAssemblyString(comp_list);
-});
+    let assembly = mabGUI.assembly;
 
-function createComponentString(component) {
-    var content = "";
-    //Append to content
-    content += "from mad import *\n";
-    content += "import time\n\n";
-    content += "class " + capitalize(component.name) + "(Component):\n";
+    for( let componentIndex = 0; componentIndex < assembly.components.length; componentIndex++ )
+    {
+        let component = assembly.components[ componentIndex ];
+        let string = componentToString( component );
+        exportComponentFile( string, component.name )
+    }
 
-    //Create places list
-    content += "\tdef create(self):\n";
-    content += "\t\tself.places = [\n";
-    for (var i = 0; i < component.place_list.length; i++) {
-        if (component.place_list[i].type === "Place") {
-            if (i == component.place_list.length - 1) {
-                content += "\t\t\t'" + component.place_list[i].name + "'\n";
-            } else {
-                content += "\t\t\t'" + component.place_list[i].name + "',\n";
-            };
-        };
-    };
-    content += "\t\t]\n\n";
+    let string = assemblyToString( assembly );
+    exportAssemblyFile( string );
 
-    //Create transitions dictionary
-    content += "\t\tself.transitions = {\n";
-    for (var j = 0; j < component.transition_list.length; j++) {
-        if (component.transition_list[j].type === "Transition") {
-            if (j == component.transition_list.length - 1) {
-                content += "\t\t\t'" + component.transition_list[j].name + "': ('" + component.transition_list[j].src.name + "', '" + component.transition_list[j].dest.name + "', self." + component.transition_list[j].func + ")\n";
-            } else {
-                content += "\t\t\t'" + component.transition_list[j].name + "': ('" + component.transition_list[j].src.name + "', '" + component.transition_list[j].dest.name + "', self." + component.transition_list[j].func + "),\n";
-            };
-        };
-    };
-    content += "\t\t}\n\n";
+}
 
-    //Create dependencies dictionary via connection list
-    content += "\t\tself.dependencies = {\n";
+function componentToString( component )
+{
 
-    if (con_list.length != 0) {
-        //First: check for provide depencencies
-        for (var k = 0; k < con_list.length; k++) {
-            if (con_list[k].provide_port_obj.component_obj.name === component.name) {
-                if (content.includes(con_list[k].provide_port_obj.name)) {
-                    continue;
-                } else {
-                    content += "\t\t\t'" + con_list[k].provide_port_obj.name + "': (DepType." + con_list[k].provide_port_obj.type + ", ['" + con_list[k].provide_port_obj.source_obj.name + "']),\n";
-                };
-            };
-        };
+    let content = '';
 
-        //Second: check for use dependencies
-        for (var l = 0; l < con_list.length; l++) {
-            if (con_list[l].use_port_obj.component_obj.name === component.name) {
-                if (content.includes(con_list[l].use_port_obj.name)) {
-                    continue;
-                } else {
-                    content += "\t\t\t'" + con_list[l].use_port_obj.name + "': (DepType." + con_list[l].use_port_obj.type + ", ['" + con_list[l].use_port_obj.source_obj.name + "']),\n";
-                };
-            };
-        };
+    content += 'from mad import *\n';
+    content += 'import time\n\n';
+    content += 'class ' + capitalize( component.name ) + '(Component):\n';
 
-        //Delete the last comma from the dependency dictionary and then append the closing bracket and new tabs/lines.
-        content = content.slice(0, -2);
-        content += "\n\t\t}\n\n";
+    content += '\tdef create( self ):\n';
+    content += '\t\tself.places = [\n';
+    for( let placeIndex = 0; placeIndex < component.places.length; placeIndex++ )
+    {
+        let place = component.places[ placeIndex ];
+        if( placeIndex == component.places.length - 1 ) {
+            content += '\t\t\t' + place.name + '\n';
+        } else {
+            content += '\t\t\t' + place.name + ',\n';
+        }
+    }
+    content += '\t\t]\n\n';
+
+    content += '\t\tself.transitions = {\n';
+    for( let transitionIndex = 0; transitionIndex < component.transitions.length; transitionIndex++ )
+    {
+        let transition = component.transitions[ transitionIndex ];
+        if( transitionIndex == component.transitions.length - 1 )
+        {
+            content += '\t\t\t' + transition.name + ': (' + transition.source.name + ', ' + transition.destination.name + ', self.' + transition.func + ')\n';
+        } else {
+            content += '\t\t\t' + transition.name + ': (' + transition.source.name + ', ' + transition.destination.name + ', self.' + transition.func + '),\n';
+        }
+    }
+    content += '\t\t}\n\n';
+
+    content += '\t\tself.dependencies = {\n';
+
+    if( component.dependencies.length != 0 )
+    {
+        for( let dependencyIndex = 0; dependencyIndex < component.dependencies.length; dependencyIndex++ )
+        {
+            let dependency = component.dependencies[ dependencyIndex ];
+            if( dependency.source.type == 'place' &&
+                !content.includes( dependency.name ) ) // provide
+            {
+                content += '\t\t\t' + dependency.name + ': (DepType.' + capitalize( dependency.serviceData ) + ', [' + dependency.source.name + '] ),\n';
+            }
+        }
+        for( let dependencyIndex = 0; dependencyIndex < component.dependencies.length; dependencyIndex++ )
+        {
+            let dependency = component.dependencies[ dependencyIndex ];
+            if( dependency.source.type == 'transition' &&
+                !content.includes( dependency.name ) ) // provide
+            {
+                content += '\t\t\t' + dependency.name + ': (DepType.' + capitalize( dependency.serviceData ) + ', [' + dependency.source.name + '] ),\n';
+            }
+        }
+        content = content.slice( 0, -2 );
+        content += '\n\t\t}\n\n';
     } else {
-        content += "\t\t}\n\n";
-    }
-    
-
-    //Create functions
-    for (var m = 0; m < component.transition_list.length; m++) {
-        if (component.transition_list[m].type === "Transition") {
-            content += "\tdef " + component.transition_list[m].func + "(self):\n";
-            content += "\t\ttime.sleep(" + getRndInteger(0, 11) + ")\n\n";
-        };
-    };
-
-    //Create the file & write to the file
-    generateCode(content, component.name);
-};
-
-//Write the content string to a file
-function generateCode(content, component_name) {
-    dialog.showSaveDialog(
-        {defaultPath: "~/" + component_name.toLowerCase() + ".py"},
-        function (fileName) {
-          // do your stuff here
-            if (fileName === undefined) {
-                console.log("You didn't save a file.");
-                return;
-            }
-            // fileName is a string that contains the path and filename created in the save file dialog.  
-            fs.writeFile(fileName, content, (err) => {
-                if (err) {
-                    alert("An error ocurred creating the file " + err.message)
-                };  
-                console.log(component_name + " has been succesfully saved!");
-            });
-      });
-};
-
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-};
-
-function createAssemblyString(comp_list) {
-    var content = "";
-    
-    //Import MAD
-    content += "from mad import *\n\n";
-    
-    //Import component files and classes
-    for (var i = 0; i < comp_list.length; i++) {
-        content += "from " + comp_list[i].name.toLowerCase() + " import " + capitalize(comp_list[i].name) + "\n";
-        if (i == comp_list.length - 1) {
-            content += "\n";
-        }
-    }
-    
-    //Add actual functionality
-    content += "if __name__ == '__main__':\n";
-    
-    //Create new classes of imported types
-    for (var j = 0; j < comp_list.length; j++) {
-        content += "\t" + comp_list[j].name.toLowerCase() + " = " + capitalize(comp_list[j].name) + "()\n\n";
+        content += '\t\t}\n\n';
     }
 
-    //Add components to the assembly
-    content += "\tassembly = Assembly()\n";
-    for (var k = 0; k < comp_list.length; k++) {
-        content += "\tassembly.addComponent('" + comp_list[k].name.toLowerCase() + "', " + comp_list[k].name.toLowerCase() + ")\n";
+    for( let transitionIndex = 0; transitionIndex < component.transitions.length; transitionIndex++ )
+    {
+        let transition = component.transitions[ transitionIndex ];
+        let sleepTime = Math.floor( Math.random( ) * ( 11 ) );
+        content += '\tdef ' + transition.func + '(self):\n';
+        content += '\t\ttime.sleep(' + sleepTime + ')\n\n';
     }
 
-    //Add connections to the assembly
-    for (var l = 0; l < con_list.length; l++) {
-        connection = con_list[l];
-        content += "\tassembly.addConnection(" + connection.provide_port_obj.component_obj.name.toLowerCase() + ", '" + connection.provide_port_obj.name + "', " + connection.use_port_obj.component_obj.name.toLowerCase() + ", '" + connection.use_port_obj.name + "')\n";
-        if (l == con_list.length - 1) {
-            content += "\n";
-        }
-    }
+    return content;
 
-    content += "\tmad = Mad(assembly)\n";
-    content += "\tmad.run()\n";
-
-    generateAssemblyCode(content);
 }
 
 //Write the content string to a file
-function generateAssemblyCode(content) {
-    dialog.showSaveDialog(
-        {defaultPath: "~/assembly.py"},
-        function (fileName) {
-          // do your stuff here
-            if (fileName === undefined) {
-                console.log("You didn't save a file.");
-                return;
+function exportComponentFile( content, componentName )
+{
+
+    fs.writeFile( componentName.toLowerCase( ) + '.py', content,
+        function( error )
+        {
+            if( error )
+            {
+                alert( 'An error occured: ' + error );
             }
-            // fileName is a string that contains the path and filename created in the save file dialog.  
-            fs.writeFile(fileName, content, (err) => {
-                if (err) {
-                    alert("An error ocurred creating the file " + err.message)
-                };  
-                console.log("The assembly has been succesfully saved!");
-            });
-      });
+        }  );
+
 };
 
-// Function to capitalize the first letter of a string
-function capitalize(string) {
-    if (typeof string !== 'string') {
-        return ''
+function assemblyToString( assembly )
+{
+    let components = assembly.components;
+    let content = '';
+
+    content += 'from mad import *\n\n';
+    for( let componentIndex = 0; componentIndex < components.length; componentIndex++ )
+    {
+        let component = components[ componentIndex ];
+        content += 'from ' + component.name.toLowerCase( ) + ' import ' + capitalize( component.name ) + '\n';
     }
-    return string.charAt(0).toUpperCase() + string.slice(1)
+    content += '\n';
+
+    content += 'if __name__ == \'__main__\':\n';
+    for( componentIndex = 0; componentIndex < components.length; componentIndex++ )
+    {
+        let component = components[ componentIndex ];
+        content += '\t' + component.name.toLowerCase( ) + ' = ' + capitalize( component.name ) + '()\n\n';
+    }
+
+    content += '\tassembly = Assembly()\n';
+    for( componentIndex = 0; componentIndex < components.length; componentIndex++ )
+    {
+        let component = components[ componentIndex ];
+        content += '\tassembly.addComponent(' + component.name.toLowerCase( ) + ', ' + component.name.toLowerCase( ) + ')\n';
+    }
+
+    for( let connectionIndex = 0; connectionIndex < assembly.connections.length; connectionIndex++ )
+    {
+        let connection = assembly.connections[ connectionIndex ];
+        content += '\tassembly.addConnection(' + connection.provide.component.name.toLowerCase( ) + ', ' + connection.provide.name + ', ' + connection.use.component.name.toLowerCase( ) + ', ' + connection.use.name + ')\n';
+    }
+    content += '\n';
+
+    content += '\tmad = Mad(assembly)\n';
+    content += '\tmad.run()\n';
+
+    return content;
+
 }
+
+//Write the content string to a file
+function exportAssemblyFile( content )
+{
+
+    fs.writeFile( 'assembly.py', content,
+        function( error )
+        {
+            if( error )
+            {
+                alert( 'An error occured: ' + error );
+            }
+        } );
+
+}
+
+function capitalize( string )
+{
+    return string.charAt( 0 ).toUpperCase( ) + string.slice( 1 );
+}
+
+module.exports = generate;
